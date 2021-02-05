a8f9826e4e8754b447d58d9fd4d060fdec037b76
InnovativeCoder <aashitsingh2010@gmail.com>
2021-02-05 13:53:33 +0530

    Done and deployed on vercel

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,10 +1,11 @@
 import Head from "next/head"
+import { useRouter } from "next/router"
 import { Toolbar } from "../../components/toolbar"
 import styles from "../../styles/Feed.module.css"
 
 export const Feed = ({pageNumber, articles}) =>{
     const urlToImage= articles.articles[pageNumber - 1].urlToImage
-    
+    const router = useRouter();
     return(
         <>
         <Head>
@@ -14,11 +15,41 @@ export const Feed = ({pageNumber, articles}) =>{
         </Head>
         <Toolbar />
         <div className={styles.main}>
-            <h1>{articles.articles[pageNumber - 1].title}</h1>
-            <h3>{articles.articles[pageNumber - 1].description}</h3>
-            <p>{articles.articles[pageNumber - 1].url}</p>
-            <img src={urlToImage} />
-            <h1>{articles.articles[pageNumber - 1].title}</h1>
+            <div className={styles.post}>
+                <h1>{articles.articles[pageNumber - 1].title}</h1>
+                <h3>{articles.articles[pageNumber - 1].description}</h3>
+                <p>{articles.articles[pageNumber - 1].url}</p>
+                <img src={urlToImage} />
+            </div>
+            <div className={styles.paginator}>
+                <div
+                onClick={()=>{
+                    if(pageNumber>1){
+                        router.push(`/feed/${pageNumber - 1}`).then(()=>{
+                            window.scrollTo(0,0)
+                        })
+                    }
+                }}
+                className={pageNumber==="1"? styles.disabled: styles.active}
+                >
+                    Previous Page
+                </div>
+                <div className={styles.number}>
+                    #{pageNumber}
+                </div>
+                <div
+                onClick={()=>{
+                    if(pageNumber<5){
+                        router.push(`/feed/${Number(pageNumber) + 1}`).then(()=>{
+                            window.scrollTo(0,0)
+                        })
+                    }
+                }}
+                className={pageNumber==="5"? styles.disabled: styles.active}
+                >
+                    Next Page
+                </div>
+            </div>
         </div>
         </>
     )
